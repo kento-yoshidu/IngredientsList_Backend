130610247e331c20d3991654fbf0d8fcd33efa99
kento <kento@toriwatari.bird>
2023-08-19 09:18:44 +0900

    🔧 Create Database

---
+++ model/dish.go
@@ -0,0 +1,19 @@
+package model
+
+import "time"
+
+type Dish struct {
+	ID        uint      `json:"id" gorm:"primaryKey"`
+	DishName  string    `json:"dish_name" gorm:"not null"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	UserId    uint      `json:"user_id" gorm:"not null"`
+	User      User      `json:"user" gorm:"foreignKey:UserId; constraint:OnDelete:CASCADE"`
+}
+
+type DishResponse struct {
+	ID        uint      `json:"id" gorm:"primaryKey"`
+	DishName  string    `json:"dish_name" gorm:"not null"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
