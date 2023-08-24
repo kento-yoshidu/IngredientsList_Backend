6fa662dc25a0c827d2de07334951637a2d11589a
kento <kento@toriwatari.bird>
2023-08-24 20:21:01 +0900

    ⚡ stockの値を返すように修正

--- usecase/ingredient_usecase.go
+++ usecase/ingredient_usecase.go
@@ -9,7 +9,6 @@ type IIngredientUsecase interface {
 	GetIngredientsByDishId(userId, dishId uint) ([]model.IngredientResponse, error)
 	CreateIngredient(ingredient model.Ingredient) (model.IngredientResponse, error)
 	/*
-		GetAllIngredients(dishId uint) ([]model.IngredientResponse, error)
 		UpdateIngredient(dishId, ingredientId uint) (model.IngredientResponse, error)
 		DeleteIngredient(dishId, ingredientId uint) error
 	*/
@@ -34,8 +33,7 @@ func (iu *ingredientUsecase) GetIngredientsByDishId(userId, dishId uint) ([]mode
 		i := model.IngredientResponse{
 			ID:             v.ID,
 			Ingredientname: v.Ingredientname,
-			CreatedAt:      v.CreatedAt,
-			UpdatedAt:      v.UpdatedAt,
+			Stock:          v.Stock,
 		}
 		resIngredients = append(resIngredients, i)
 	}
@@ -58,25 +56,3 @@ func (iu *ingredientUsecase) CreateIngredient(ingredient model.Ingredient) (mode
 
 	return resIngredient, nil
 }
-
-/*
-func (iu *ingredientUsecase) GetAllIngredients(dishId uint) ([]model.IngredientResponse, error) {
-	ingredients := []model.Ingredient{}
-	if err := iu.ir.GetAllIngredients(&ingredients, dishId); err != nil {
-		return nil, err
-	}
-
-	resIngredients := []model.IngredientResponse{}
-	for _, v := range ingredients {
-		ingredient := model.IngredientResponse{
-			ID:             v.ID,
-			Ingredientname: v.Ingredientname,
-			CreatedAt:      v.CreatedAt,
-			UpdatedAt:      v.UpdatedAt,
-		}
-		resIngredients = append(resIngredients, ingredient)
-	}
-
-	return resIngredients, nil
-}
-*/
